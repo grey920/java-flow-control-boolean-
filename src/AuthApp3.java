a9d96f14bcb358e0d6a4e41d84153647ab87f51c
grey920 <charminggw@gmail.com>
2020-01-09 11:22:42 +0900

    제어문 - 마지막

---
+++ src/AuthApp3.java
@@ -0,0 +1,36 @@
+
+public class AuthApp3 {
+
+	public static void main(String[] args) {
+		
+		//String[] users = {"grey","dawa","egoing"};
+		String[][] users = {
+				{"grey", "1111"},
+				{"dawa", "2222"},
+				{"egoing", "3333"}
+		};
+		String inputId = args[0];
+		String inputPass = args[1];
+		
+		boolean isLogined = false;
+		for(int i=0; i<users.length; i++) {
+			String[] current = users[i];
+			if(
+					current[0].equals(inputId) &&
+					current[1].equals(inputPass)
+			) {
+				isLogined = true;
+				break;
+			}
+		}
+		System.out.println("Hi,");
+		if(isLogined) {
+			System.out.println("Master!!");
+		} else {
+			System.out.println("Who are you?");
+		}
+		
+		
+	}
+
+}
--- src/LoopArray.java
+++ src/LoopArray.java
@@ -2,8 +2,21 @@
 public class LoopArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 *  <li>grey</li>
+		 *  <li>egoing</li>
+		 *  <li>dawa</li>
+		 */
+		
+		String[] users = new String[3];
+		users[0] = "grey";
+		users[1] = "egoing";
+		users[2] = "dawa";
+		
+		for(int i=0; i<users.length; i++) {
+			System.out.println(users[i]+",");
+		}
+		
 	}
 
 }
