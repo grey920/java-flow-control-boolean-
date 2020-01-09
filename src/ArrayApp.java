0752905446b5c62bf4985d4f0fd466cc502d63dc
grey920 <charminggw@gmail.com>
2020-01-09 10:34:12 +0900

    반복문+배열

---
+++ src/ArrayApp.java
@@ -0,0 +1,21 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+		
+		// grey, egoing, dawa
+//		String users = "grey, egoing, dawa"
+		String[] users = new String[3];
+		users[0] = "grey";
+		users[1] = "egoing";
+		users[2] = "dawa";
+		
+		System.out.println(users[1]);
+		System.out.println(users.length);
+		
+		int[] scores = {10, 100, 100};
+		System.out.println(scores.length);
+		
+	}
+
+}
---
+++ src/LoopArray.java
@@ -0,0 +1,9 @@
+
+public class LoopArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
